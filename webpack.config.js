10feed7ca496fed329ea208959027e24ae297476
Hay I'am Inces <74086121+fitri612@users.noreply.github.com>
2022-08-21 20:17:24 +0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+
+const include = path.join(__dirname, 'src')
+
+module.exports = {
+  devtool: 'source-map',
+  mode: 'development',
+  entry: ['./src/index'],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  devServer: {
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loaders: [
+          {
+            loader: 'babel-loader',
+            options: {
+              babelrc: false,
+              presets: ['@babel/preset-react', '@babel/preset-env']
+            }
+          }
+        ],
+        include
+      },
+      {
+        test: /\.jpg/,
+        loaders: ['file-loader'],
+        include
+      }
+    ]
+  }
+}
+
+// module.exports = {
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/i,
+//         use: ['style-loader', 'css-loader'],
+//       },
+//     ],
+//   },
+// };
